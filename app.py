7d727fc6fb59be67fb5185bcca34f0677b00b876
김선호 <kimhot666@gmail.com>
2026-08-20 15:26:02 +0900

    Add C,R with DB

--- app.py
+++ app.py
@@ -1,59 +1,66 @@
 from flask import Flask, request
+import sqlite3
 
 app = Flask(__name__)
 
-issue_store = []
-next_issue_id = 0
+conn = sqlite3.connect('issues.db')
+cursor = conn.cursor()
 
-@app.route("/health")
-def health():
-  return {"status":"ok"}
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS issues(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT NOT NULL,
+  status TEXT NOT NULL
+)
+""")
 
 @app.route("/issues")
 def issues():
-  return issue_store
+  rows = cursor.execute("SELECT * FROM issues").fetchall()
+  return rows
 
 @app.route("/issues", methods=["POST"])
 def post_issue():
-  global next_issue_id
   data = request.get_json()
   if data.get('title'):
+    status = data.get('status', 'open')
+    cursor.execute("INSERT INTO issues (title, status) VALUES (?,?)", (data.get('title'), status))
+    conn.commit()
+    row = cursor.execute("SELECT * FROM issues WHERE id = ?", (cursor.lastrowid,)).fetchone()
     issue = {
-      'id': next_issue_id,
-      'title': data.get('title'),
-      'status': data.get('status', 'open')
+      'id': row[0],
+      'title': row[1],
+      'status': row[2]
     }
-    issue_store.append(issue)
-    next_issue_id += 1
     return issue, 201
   return {'error': 'No Title'}, 400
 
 @app.route("/issues/<int:issue_id>")
 def get_issue(issue_id):
-  for issue in issue_store:
-    if issue['id'] == issue_id:
-      return issue
+  issue = cursor.execute("SELECT * FROM issues WHERE id = ?", issue_id)
+  if issue:
+    return issue
   return {'error': 'Not Found'}, 404
 
-@app.route('/issues/<int:issue_id>', methods=['DELETE'])
-def delete_issue(issue_id):
-  for issue in issue_store:
-    if issue['id'] == issue_id:
-      issue_store.remove(issue)
-      return '', 204
-  return {'error': 'Not Found'}, 404
+# @app.route('/issues/<int:issue_id>', methods=['DELETE'])
+# def delete_issue(issue_id):
+#   for issue in issue_store:
+#     if issue['id'] == issue_id:
+#       issue_store.remove(issue)
+#       return '', 204
+#   return {'error': 'Not Found'}, 404
 
-@app.route('/issues/<int:issue_id>', methods=['PATCH'])
-def patch_issue(issue_id):
-  for issue in issue_store:
-    if issue['id'] == issue_id:
-      data = request.get_json()
-      if 'title' in data:
-        issue['title'] = data['title']
-      if 'status' in data:
-        issue['status'] = data['status']
-      return issue
-  return {'error': 'Not Found'}, 404
+# @app.route('/issues/<int:issue_id>', methods=['PATCH'])
+# def patch_issue(issue_id):
+#   for issue in issue_store:
+#     if issue['id'] == issue_id:
+#       data = request.get_json()
+#       if 'title' in data:
+#         issue['title'] = data['title']
+#       if 'status' in data:
+#         issue['status'] = data['status']
+#       return issue
+#   return {'error': 'Not Found'}, 404
 
 if __name__ == "__main__":
   app.run()
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,21 +2,18 @@ import app as app_module
 import pytest
 
 client = app_module.app.test_client()
+conn = app_module.conn
+cursor = app_module.conn.cursor()
 
 @pytest.fixture
 def reset_state():
-  app_module.next_issue_id = 0
-  app_module.issue_store.clear()
-
-def test_health(reset_state):
-  response = client.get("/health")
-  assert response.status_code == 200
-  assert response.json["status"] == "ok"
-
-def test_issues(reset_state):
-  response = client.get("/issues")
-  assert response.status_code == 200
-  assert response.json == []
+  cursor.execute("DELETE FROM issues")
+  conn.commit()
+  
+# def test_issues(reset_state):
+#   response = client.get("/issues")
+#   assert response.status_code == 200
+#   assert response.json == []
 
 def test_post_issue(reset_state):
   payload = {'title': 'Fix login bug'}
@@ -25,53 +22,54 @@ def test_post_issue(reset_state):
   assert response.json['title'] == 'Fix login bug'
   assert response.json['status'] == 'open'
   response_get = client.get('/issues')
-  assert response_get.json[0]['title'] == 'Fix login bug'
+  assert response_get.json[0][1] == 'Fix login bug'
 
 def test_post_issue_without_title(reset_state):
   payload = {}
   response = client.post('/issues', json=payload)
   assert response.status_code == 400
   assert response.json['error'] == 'No Title'
-  assert app_module.next_issue_id == 0
-  assert app_module.issue_store == []
+  cursor.execute("SELECT COUNT(*) FROM issues")
+  row_count = cursor.fetchone()[0]
+  assert row_count == 0
   
-def test_get_issue(reset_state):
-  payload = {'title': 'Fix login bug'}
-  response_post = client.post('/issues', json = payload)
-  assert response_post.status_code == 201
-  response = client.get('/issues/0')
-  assert response.status_code == 200
-  assert response.json['title'] == 'Fix login bug'
+# def test_get_issue(reset_state):
+#   payload = {'title': 'Fix login bug'}
+#   response_post = client.post('/issues', json = payload)
+#   assert response_post.status_code == 201
+#   response = client.get('/issues/0')
+#   assert response.status_code == 200
+#   assert response.json['title'] == 'Fix login bug'
   
-def test_get_non_existent_issue(reset_state):
-  response = client.get('/issues/50')
-  assert response.status_code == 404
-  assert response.json['error'] == 'Not Found'
+# def test_get_non_existent_issue(reset_state):
+#   response = client.get('/issues/50')
+#   assert response.status_code == 404
+#   assert response.json['error'] == 'Not Found'
 
-def test_delete_issue(reset_state):
-  payload = {'title': 'Fix login bug'}
-  response = client.post('/issues', json = payload)
-  assert response.status_code == 201
-  assert response.json['title'] == 'Fix login bug'
-  response = client.delete('/issues/0')
-  assert response.status_code == 204
-  response = client.get('/issues/0')
-  assert response.status_code == 404
-  assert response.json['error'] == 'Not Found'
-  assert app_module.issue_store == []
+# def test_delete_issue(reset_state):
+#   payload = {'title': 'Fix login bug'}
+#   response = client.post('/issues', json = payload)
+#   assert response.status_code == 201
+#   assert response.json['title'] == 'Fix login bug'
+#   response = client.delete('/issues/0')
+#   assert response.status_code == 204
+#   response = client.get('/issues/0')
+#   assert response.status_code == 404
+#   assert response.json['error'] == 'Not Found'
+#   assert app_module.issue_store == []
   
-def test_patch_issue(reset_state):
-  payload = {'title': 'Fix login bug'}
-  response = client.post('/issues', json=payload)
-  assert response.status_code == 201
-  assert response.json['title'] == 'Fix login bug'
-  assert response.json['status'] == 'open'
-  payload = {'status': 'closed'}
-  response = client.patch('/issues/0', json=payload)
-  assert response.status_code == 200
-  assert response.json['status'] == 'closed'
-  assert response.json['title'] == 'Fix login bug'
-  response = client.get('/issues/0')
-  assert response.json['status'] == 'closed'
-  response = client.patch('/issues/999')
-  assert response.status_code == 404
+# def test_patch_issue(reset_state):
+#   payload = {'title': 'Fix login bug'}
+#   response = client.post('/issues', json=payload)
+#   assert response.status_code == 201
+#   assert response.json['title'] == 'Fix login bug'
+#   assert response.json['status'] == 'open'
+#   payload = {'status': 'closed'}
+#   response = client.patch('/issues/0', json=payload)
+#   assert response.status_code == 200
+#   assert response.json['status'] == 'closed'
+#   assert response.json['title'] == 'Fix login bug'
+#   response = client.get('/issues/0')
+#   assert response.json['status'] == 'closed'
+#   response = client.patch('/issues/999')
+#   assert response.status_code == 404
